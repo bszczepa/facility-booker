28034a06b03b53a22b078a3643c9c7a58246c7a5
Aleksander Szczepanski <aleksander.szczepanski@outlook.com>
2020-06-09 11:37:07 +0200

    added reservation controller

--- src/main/java/com/student/facilityBooking/controllers/ReservationRestController.java
+++ src/main/java/com/student/facilityBooking/controllers/ReservationRestController.java
@@ -1,6 +1,7 @@
 package com.student.facilityBooking.controllers;
 
-mport com.student.facilityBooking.persistence.ActivitiesService;
+import com.student.facilityBooking.model.Activities;
+import com.student.facilityBooking.persistence.ActivitiesService;
 import com.student.facilityBooking.model.Facility;
 import com.student.facilityBooking.model.Reservation;
 import com.student.facilityBooking.persistence.ActivitiesService;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/reservations")
+@RequestMapping("/api/reservation")
 public class ReservationRestController {
 
     @Autowired
@@ -25,6 +26,12 @@ public class ReservationRestController {
 
     @Autowired
     ActivitiesService activitiesService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getAll() {
+        Collection<Reservation> reservations = reservationService.getAll();
+        return new ResponseEntity<Collection<Reservation>>(reservations, HttpStatus.OK);
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<?> saveResource(@RequestBody Reservation reservation, @PathVariable("id") long id){
